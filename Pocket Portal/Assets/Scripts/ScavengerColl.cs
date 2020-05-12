b9c412adcac6c404c32909d4bc3e3e2b6d614bb1
Ryan Earles <rjearles@knox.edu>
2020-05-12 16:52:28 -0600

    Scavenger Hunt Destroy and Sound

---
+++ Pocket Portal/Assets/Scripts/ScavengerColl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScavengerColl : MonoBehaviour
+{
+public AudioClip sound;
+private AudioSource audioSource;
+
+void Start ()   
+     {
+         GetComponent<AudioSource> ().playOnAwake = false;
+     }  
+
+
+void OnTriggerEnter(Collider other)
+{
+    if (other.gameObject.tag == "MainCamera")
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = sound;
+        audioSource.Play();
+        Destroy(gameObject, 0.5f);
+    }
+}
+
+}
+
