2e81b866975fed48ab6be0114f2521203ba387a7
Ryan Earles <rjearles@knox.edu>
2020-05-14 17:16:52 -0600

    Particle effect works sort of

--- Pocket Portal/Assets/Scripts/ScavengerColl.cs
+++ Pocket Portal/Assets/Scripts/ScavengerColl.cs
@@ -6,13 +6,6 @@ public class ScavengerColl : MonoBehaviour
 {
 public AudioClip sound;
 private AudioSource audioSource;
-public ParticleSystem _psystem;
-
-void Start ()   
-     {
-         GetComponent<AudioSource> ().playOnAwake = false;
-         _psystem = GetComponent<ParticleSystem>();
-     }  
 
 
 void OnTriggerEnter(Collider other)
@@ -22,7 +15,6 @@ void OnTriggerEnter(Collider other)
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = sound;
         audioSource.Play();
-        _psystem.Play();
         Destroy(gameObject, 0.5f);
     }
 }
