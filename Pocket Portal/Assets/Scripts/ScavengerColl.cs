d69e56a2fa1cb8e63351ea84ab143370ce3a3faf
eschartz <ejschartz@knox.edu>
2020-05-21 18:39:54 -0500

    Fixing UI bugs
    added home button, fixed close button, added button noises, added background behind object counter.

--- Pocket Portal/Assets/Scripts/ScavengerColl.cs
+++ Pocket Portal/Assets/Scripts/ScavengerColl.cs
@@ -5,47 +5,55 @@ using UnityEngine.UI;
 
 public class ScavengerColl : MonoBehaviour
 {
-public AudioClip sound;
-private AudioSource audioSource;
-private static int Score = 0;
-private int winScore = 3; //max items to find
-public Text ScoreText;
-public GameObject winPanel;
+	public AudioClip sound;
+	private AudioSource audioSource;
+	private static int Score = 0;
+	private int winScore = 3; //max items to find
+	public Text ScoreText;
+	public GameObject winPanel;
 
-void Start ()
+	void Start()
 	{
 		//Score = 0;
-		SetScoreText ();
+		SetScoreText();
 	}
 
 
-void OnTriggerEnter(Collider other)
-{
-    if (other.gameObject.tag == "MainCamera")
-    {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = sound;
-        audioSource.Play();
-        Destroy(gameObject, 0.65f);
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "MainCamera")
+		{
+			audioSource = GetComponent<AudioSource>();
+			audioSource.clip = sound;
+			audioSource.Play();
+			Destroy(gameObject, 0.75f);
 
 			//Score update
-        Score = Score + 1;
-		SetScoreText ();
+			Score = Score + 1;
+			SetScoreText();
 
-        if(Score == winScore) //opening win panel
-		{
-			winPanel.SetActive(true);
+			if (Score == winScore) //opening win panel
+			{
+				StartCoroutine(OpenWinScreen());
+			}
 		}
-    }
-}
+	}
 
-void SetScoreText()
+	void SetScoreText()
 	{
 		ScoreText.text = Score.ToString() + " / " + winScore;
 	}
 
+	IEnumerator OpenWinScreen()
+	{
+		yield return new WaitForSeconds(0.7f); //wait for particle effect to play
+		winPanel.SetActive(true); //open win screen
+
+	}
+
+
 }
 
- 
-         
- 
+
+
+
